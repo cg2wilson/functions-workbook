a6ed8f9d67f9183a4d511f79720aa30f8ed68344
cg2wilson <cory.g.wilson@gmail.com>
2026-05-27 12:57:42 -0500

    finish section 1.1 and add section 1.2 shell

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,140 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec1-1",
+  "level": "1",
+  "url": "sec1-1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Section 1.1",
+  "body": " Section 1.1   Relation   A relation is a rule which establishes a relationship between two variables.     Function   A function is a relation that assigns to each element of one set (called the domain ) exactly one element of the a second set (called the range ).  Elements of the domain are called inputs or independent variables and elements of the range are called outputs or dependent variables .  A function may be defined by a set of ordered pairs, a table, a graph, an equation, or a verbal description.      A local clothing store begins selling merchandise for a popular local sports team. The sales are given in the table below.   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold (in hundreds)  10.7  16.9  21.8  30.4  34.6  42.1     If we take \"days\" to be our inputs and \"items sold\" to be our outputs, then the table shows a relation between the two variables. This relation is a function because each input has only one output paired with it.  We can use the data to form a scatterplot - a plot of input\/output pairs on a coordinate system:   A scatterplot showing the relation in   A graphical representation of the table in      In the scatterplot, the independent variable is \"days\" while the dependent variable is \"items sold\".  If we make \"days\" the dependent variable and \"items sold\" the independent variable instead, the scatterplot looks like this:   A second scatterplot showing the relation in   A second graphical representation of the table in         In the previous example, the outputs were given in terms of hundred items sold - this means that the outputs should be read as, for example, \"10.7 hundred items sold\".  Since this is not how we might normally think about numbers, it may be helpful to think about multiplying by 100 to get the actual number of items sold. This slightly changes the table to the following:   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold  1070  1690  2180  3040  3460  4210        The following are all examples of functions:                   You can check that these are functions because every input has only one output. But how do we know which is the input and which is the output?  The output is usually written as either a single variable (like or above) or by using function notation.     Function Notation  For a function whose inputs are represented by the variable , and whose outputs are represented by the variable , the the function notation for this relationship is shown as   This is read as \" of \".     Let's return to the data from .   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold (in hundreds)  10.7  16.9  21.8  30.4  34.6  42.1     If we let represent for 'items sold' and let represent the days since the merchandise sales began, then our function is given by the notation . Here, is the independent variable and is the dependent variable.  The notation instructs us to look for the input and find the corresponding output paired with . In this case, hundred items sold. Following , we could also write items sold. Knowing which one to use depends entirely on the question- be careful!      Use and to the following questions.   Write the function notation for the input\/output pair     Write a brief sentence translating the notation from math to English.    For which input is it true that ?               An example sentence might be \"Twelve days after merchandise began selling, 42.1 hundred items were sold.\". Anything that is syntactically equivalent is acceptable.    This occurs when .         We can also find function values when given a rule or graph.  Consider the function . The statement says that the input variable, which we represent by , should be replaced by the number . So, Thus,   We can use the graph to find similar information.   The graph of   The graph of     We can find by first finding on the horizontal axis, then finding the corresponding point on the function.   Finding a point on a function    Locating on the horizontal axis      Locating on the function         Let and be given by the graph below.   The graph of   The graph of      Answer the following:   What is     Find     For what vaue is ?    What is ?                                 "
+},
+{
+  "id": "def-relation",
+  "level": "2",
+  "url": "sec1-1.html#def-relation",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "Relation.",
+  "body": " Relation   A relation is a rule which establishes a relationship between two variables.   "
+},
+{
+  "id": "def-function",
+  "level": "2",
+  "url": "sec1-1.html#def-function",
+  "type": "Definition",
+  "number": "1.1.2",
+  "title": "Function.",
+  "body": " Function   A function is a relation that assigns to each element of one set (called the domain ) exactly one element of the a second set (called the range ).  Elements of the domain are called inputs or independent variables and elements of the range are called outputs or dependent variables .  A function may be defined by a set of ordered pairs, a table, a graph, an equation, or a verbal description.   "
+},
+{
+  "id": "sec1-1-function-ex1",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-function-ex1",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "",
+  "body": "  A local clothing store begins selling merchandise for a popular local sports team. The sales are given in the table below.   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold (in hundreds)  10.7  16.9  21.8  30.4  34.6  42.1     If we take \"days\" to be our inputs and \"items sold\" to be our outputs, then the table shows a relation between the two variables. This relation is a function because each input has only one output paired with it.  We can use the data to form a scatterplot - a plot of input\/output pairs on a coordinate system:   A scatterplot showing the relation in   A graphical representation of the table in      In the scatterplot, the independent variable is \"days\" while the dependent variable is \"items sold\".  If we make \"days\" the dependent variable and \"items sold\" the independent variable instead, the scatterplot looks like this:   A second scatterplot showing the relation in   A second graphical representation of the table in       "
+},
+{
+  "id": "sec1-1-function-ex1-note",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-function-ex1-note",
+  "type": "Note",
+  "number": "1.1.7",
+  "title": "",
+  "body": " In the previous example, the outputs were given in terms of hundred items sold - this means that the outputs should be read as, for example, \"10.7 hundred items sold\".  Since this is not how we might normally think about numbers, it may be helpful to think about multiplying by 100 to get the actual number of items sold. This slightly changes the table to the following:   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold  1070  1690  2180  3040  3460  4210     "
+},
+{
+  "id": "sec1-1-function-ex2",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-function-ex2",
+  "type": "Example",
+  "number": "1.1.9",
+  "title": "",
+  "body": "  The following are all examples of functions:                   You can check that these are functions because every input has only one output. But how do we know which is the input and which is the output?  The output is usually written as either a single variable (like or above) or by using function notation.   "
+},
+{
+  "id": "sec1-1-function-notation-2",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-function-notation-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "function notation "
+},
+{
+  "id": "sec1-1-notation-ex1",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-notation-ex1",
+  "type": "Example",
+  "number": "1.1.10",
+  "title": "",
+  "body": "  Let's return to the data from .   Sales for a Local Clothing Store    Day  2  4  6  8  10  12    Items Sold (in hundreds)  10.7  16.9  21.8  30.4  34.6  42.1     If we let represent for 'items sold' and let represent the days since the merchandise sales began, then our function is given by the notation . Here, is the independent variable and is the dependent variable.  The notation instructs us to look for the input and find the corresponding output paired with . In this case, hundred items sold. Following , we could also write items sold. Knowing which one to use depends entirely on the question- be careful!   "
+},
+{
+  "id": "sec1-1-notation-cu1",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-notation-cu1",
+  "type": "Check Your Understanding",
+  "number": "1.1.12",
+  "title": "",
+  "body": "  Use and to the following questions.   Write the function notation for the input\/output pair     Write a brief sentence translating the notation from math to English.    For which input is it true that ?               An example sentence might be \"Twelve days after merchandise began selling, 42.1 hundred items were sold.\". Anything that is syntactically equivalent is acceptable.    This occurs when .      "
+},
+{
+  "id": "sec1-1-notation-ex2",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-notation-ex2",
+  "type": "Example",
+  "number": "1.1.13",
+  "title": "",
+  "body": "  We can also find function values when given a rule or graph.  Consider the function . The statement says that the input variable, which we represent by , should be replaced by the number . So, Thus,   We can use the graph to find similar information.   The graph of   The graph of     We can find by first finding on the horizontal axis, then finding the corresponding point on the function.   Finding a point on a function    Locating on the horizontal axis      Locating on the function      "
+},
+{
+  "id": "sec1-1-notation-cu2",
+  "level": "2",
+  "url": "sec1-1.html#sec1-1-notation-cu2",
+  "type": "Check Your Understanding",
+  "number": "1.1.16",
+  "title": "",
+  "body": "  Let and be given by the graph below.   The graph of   The graph of      Answer the following:   What is     Find     For what vaue is ?    What is ?                                "
+},
+{
+  "id": "sec1-2",
+  "level": "1",
+  "url": "sec1-2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Section 1.2",
+  "body": " Section 1.2    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
