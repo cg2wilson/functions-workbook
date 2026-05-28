ed65316cd25875b5d66899df86cfbdb595e378fb
cg2wilson <cory.g.wilson@gmail.com>
2026-05-27 21:30:46 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -226,6 +226,141 @@ var ptx_lunr_docs = [
   "body": "  Estimate the real zeros of the function    The graph of   The complete graph of        The roots are at approximately , , and    "
 },
 {
+  "id": "sec2-1",
+  "level": "1",
+  "url": "sec2-1.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Section 2.1",
+  "body": " Section 2.1   Literal Equation   A literal equation is an equation that contains two or more letters that represent constants or variables.     Solving Literal Equations  To solve a literal equation for a specific variable, use the order of operations to isolate the variable.     For the literal equation , if we want to solve for , we need to divide both sides by :   If we want to solve for , then we need to get by itself by dividing both sides by :       For more complicated literal equations, we need to follow the order of operations more closely. Consider the equation , and suppose we want to solve for .  One way to solve the equation is by distributing the through to both terms on the inside of the parentheses to get Now we need to get as isolated as possible. Subtract the from both sides to get Now since is multiplying , we can divide both sides by to get   A second way to solve the equation is by noticing that if we divide both sides by , we can avoid some of the algebra from solution one: Now subtract from both sides: Now divide by to get the algebraically equivalent answer       Solve the equation for and then for     When solving for , we get or   Solving for , we get       A direct proportion is a relationship between two variables and such that for some value (called the proportionality constant )      Suppose that the cost of a used car, including a 8% sales tax on the price of the car, is $12,500. How much of the total cost of this car is sales tax?  The total price, , is made up of two pieces: , the price of the car without tax, and , the amount of tax paid on the price of the car.  We know that the total is $12,500 and that the tax rate is 8%. This means that 8% of must be the tax paid. So,   Now, , so that . To solve for , the price of the car without tax, we divide by to get So, the price of the car was $11,574.07.      A company lost a lawsuit and was made to pay 7.4 million dollars to the plaintiff. If this amount was 125% of the company's revenue in the 2025 fiscal year, how much revenue did the company make in FY2025?    5.92 million dollars.    "
+},
+{
+  "id": "def-literaleqn",
+  "level": "2",
+  "url": "sec2-1.html#def-literaleqn",
+  "type": "Definition",
+  "number": "1.3.1",
+  "title": "Literal Equation.",
+  "body": " Literal Equation   A literal equation is an equation that contains two or more letters that represent constants or variables.   "
+},
+{
+  "id": "sec2-1-literal-ex1",
+  "level": "2",
+  "url": "sec2-1.html#sec2-1-literal-ex1",
+  "type": "Example",
+  "number": "1.3.2",
+  "title": "",
+  "body": "  For the literal equation , if we want to solve for , we need to divide both sides by :   If we want to solve for , then we need to get by itself by dividing both sides by :    "
+},
+{
+  "id": "sec2-1-literal-ex2",
+  "level": "2",
+  "url": "sec2-1.html#sec2-1-literal-ex2",
+  "type": "Example",
+  "number": "1.3.3",
+  "title": "",
+  "body": "  For more complicated literal equations, we need to follow the order of operations more closely. Consider the equation , and suppose we want to solve for .  One way to solve the equation is by distributing the through to both terms on the inside of the parentheses to get Now we need to get as isolated as possible. Subtract the from both sides to get Now since is multiplying , we can divide both sides by to get   A second way to solve the equation is by noticing that if we divide both sides by , we can avoid some of the algebra from solution one: Now subtract from both sides: Now divide by to get the algebraically equivalent answer    "
+},
+{
+  "id": "sec2-1-literal-cu1",
+  "level": "2",
+  "url": "sec2-1.html#sec2-1-literal-cu1",
+  "type": "Check Your Understanding",
+  "number": "1.3.4",
+  "title": "",
+  "body": "  Solve the equation for and then for     When solving for , we get or   Solving for , we get    "
+},
+{
+  "id": "def-directproportion",
+  "level": "2",
+  "url": "sec2-1.html#def-directproportion",
+  "type": "Definition",
+  "number": "1.3.5",
+  "title": "",
+  "body": "  A direct proportion is a relationship between two variables and such that for some value (called the proportionality constant )   "
+},
+{
+  "id": "sec2-1-proportion-ex1",
+  "level": "2",
+  "url": "sec2-1.html#sec2-1-proportion-ex1",
+  "type": "Example",
+  "number": "1.3.6",
+  "title": "",
+  "body": "  Suppose that the cost of a used car, including a 8% sales tax on the price of the car, is $12,500. How much of the total cost of this car is sales tax?  The total price, , is made up of two pieces: , the price of the car without tax, and , the amount of tax paid on the price of the car.  We know that the total is $12,500 and that the tax rate is 8%. This means that 8% of must be the tax paid. So,   Now, , so that . To solve for , the price of the car without tax, we divide by to get So, the price of the car was $11,574.07.   "
+},
+{
+  "id": "sec2-1-proportion-cu1",
+  "level": "2",
+  "url": "sec2-1.html#sec2-1-proportion-cu1",
+  "type": "Check Your Understanding",
+  "number": "1.3.7",
+  "title": "",
+  "body": "  A company lost a lawsuit and was made to pay 7.4 million dollars to the plaintiff. If this amount was 125% of the company's revenue in the 2025 fiscal year, how much revenue did the company make in FY2025?    5.92 million dollars.   "
+},
+{
+  "id": "sec1-342-2",
+  "level": "1",
+  "url": "sec1-342-2.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Section 1.3\/1.4\/2.2",
+  "body": " Section 1.3\/1.4\/2.2   Linear Function   A linear function is a function which can be written in the form where and are constants.     Intercepts   An intercept is the location where a function crosses an axis.  An intercept is an input where the function crosses the axis, and a intercept is an output where the function crosses the axis.     Because intercepts occur when a function crosses the axis, this means that all outputs are zero. In particular, this means that all intercepts are solutions to the equation .  Similarly, the intercept occurs at because the input is 0 along the axis. A function can only have one  intercept by definition of a function (see ).    Slope   The slope of a linear function is defined as the ratio of vertical change to horizontal change. This can be understood to be \"rise over run\" of the function.  The slope of a linear equation is the value of the constant .     Constant Rate of Change   The constant rate of change of a linear function is the value of its slope.      Consider the equation . It is linear because we can isolate and write the equation in the form ; rewriting gives   If we want to find the intercept, we can do so by setting . This gives: So the intercept occurs at .  For the intercept, we need to find . This means we plug in 0 to the equation:   Since the slope is the value of the constant when the equation is in the form , and we know that , the slope is . This tells us that when creating the line, to move from one point to another, we must go down two units and right five units.    Things to discuss:   constant functions    slope-intercept form    point-slope form    equation of vertical\/horizontal lines    profit, revenue, cost    average rate of change    computing linear regressions     "
+},
+{
+  "id": "def-linear-function",
+  "level": "2",
+  "url": "sec1-342-2.html#def-linear-function",
+  "type": "Definition",
+  "number": "1.4.1",
+  "title": "Linear Function.",
+  "body": " Linear Function   A linear function is a function which can be written in the form where and are constants.   "
+},
+{
+  "id": "def-intercepts",
+  "level": "2",
+  "url": "sec1-342-2.html#def-intercepts",
+  "type": "Definition",
+  "number": "1.4.2",
+  "title": "Intercepts.",
+  "body": " Intercepts   An intercept is the location where a function crosses an axis.  An intercept is an input where the function crosses the axis, and a intercept is an output where the function crosses the axis.   "
+},
+{
+  "id": "sec1-342-2-intercepts-note-1",
+  "level": "2",
+  "url": "sec1-342-2.html#sec1-342-2-intercepts-note-1",
+  "type": "Note",
+  "number": "1.4.3",
+  "title": "",
+  "body": " Because intercepts occur when a function crosses the axis, this means that all outputs are zero. In particular, this means that all intercepts are solutions to the equation .  Similarly, the intercept occurs at because the input is 0 along the axis. A function can only have one  intercept by definition of a function (see ).  "
+},
+{
+  "id": "def-slope",
+  "level": "2",
+  "url": "sec1-342-2.html#def-slope",
+  "type": "Definition",
+  "number": "1.4.4",
+  "title": "Slope.",
+  "body": " Slope   The slope of a linear function is defined as the ratio of vertical change to horizontal change. This can be understood to be \"rise over run\" of the function.  The slope of a linear equation is the value of the constant .   "
+},
+{
+  "id": "def-constant-roc",
+  "level": "2",
+  "url": "sec1-342-2.html#def-constant-roc",
+  "type": "Definition",
+  "number": "1.4.5",
+  "title": "Constant Rate of Change.",
+  "body": " Constant Rate of Change   The constant rate of change of a linear function is the value of its slope.   "
+},
+{
+  "id": "sec1-342-2-linear-ex1",
+  "level": "2",
+  "url": "sec1-342-2.html#sec1-342-2-linear-ex1",
+  "type": "Example",
+  "number": "1.4.6",
+  "title": "",
+  "body": "  Consider the equation . It is linear because we can isolate and write the equation in the form ; rewriting gives   If we want to find the intercept, we can do so by setting . This gives: So the intercept occurs at .  For the intercept, we need to find . This means we plug in 0 to the equation:   Since the slope is the value of the constant when the equation is in the form , and we know that , the slope is . This tells us that when creating the line, to move from one point to another, we must go down two units and right five units.   "
+},
+{
   "id": "backmatter-2",
   "level": "1",
   "url": "backmatter-2.html",
